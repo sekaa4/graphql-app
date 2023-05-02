a156a84de98d07786856d4da8cb582587bc36343
Sergey P <s.pansevi4@gmail.com>
2023-05-03 00:18:17 +0300

    fix: change path in lint-staged.config file

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  'src/**/*.{json,css,scss,md}': (filenames) => ['npm run format:fix', 'npm run validate'],
-  'src/**/*.{js,jsx,ts,tsx}': (filenames) => [
+  '**/*.{json,css,scss,md}': (filenames) => ['npm run format:fix', 'npm run validate'],
+  '**/*.{js,jsx,ts,tsx}': (filenames) => [
     'npm run format:fix',
     'npm run lint',
     'npm run type:check',

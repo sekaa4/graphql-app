7ca5456a2597b60bb447881cc9394af6f9e9ccb9
Sergey P <s.pansevi4@gmail.com>
2023-05-11 13:33:08 +0300

    fix: add update components SideBar&CustomSchema into index.tsx file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,25 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 
 import { auth, logout } from '@/app/components/FireBase';
 import { CustomSchema } from '@/entities/CustomSchema';
-import { DocumentSchemaLazy } from '@/entities/SideBar/ui/DocumentSchema.lazy';
+import { fetchSchemaByAPI } from '@/features/SideBar/api/shemaByAnyAPI';
+import { DocumentSchemaLazy } from '@/features/SideBar/ui/DocumentSchema.lazy';
 import homeStyles from '@/pages/home.module.css';
 import { getCoreServerSideProps, SSRPageProps } from '@/shared/lib/ssr';
 import { LangSwitcher } from '@/shared/ui/LangSwitcher/LangSwitcher';
 import { Sidebar } from '@/widgets/layouts/side-bar';
 
+const GRAPHQL_END_POINT_SCHEMA = 'https://rickandmortyapi.com/graphql';
+
 const Home = (props: SSRPageProps) => {
   const [user, loading, error] = useAuthState(auth);
   const router = useRouter();
   const { t } = useTranslation('common');
+
+  const [getSchemaByAPI, { data: currentSchema, error: errorAPI, isLoading }] = fetchSchemaByAPI();
+
+  useEffect(() => {
+    getSchemaByAPI(GRAPHQL_END_POINT_SCHEMA);
+  }, [getSchemaByAPI]);
 
   useEffect(() => {
     if (!user) router.push('/welcome');
@@ -35,11 +44,10 @@ const Home = (props: SSRPageProps) => {
         </div>
         <div></div>
       </main>
-      {t('h1')}
       {/* need draw when click on icon */}
       <Suspense fallback={<div>Loading...</div>}>
         <DocumentSchemaLazy>
-          <CustomSchema />
+          <CustomSchema schema={currentSchema} />
         </DocumentSchemaLazy>
       </Suspense>
     </>

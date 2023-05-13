bc8513b37145ca580f6dbad4c3bc0a595915a089
AntonFartovii <anton@rza.by>
2023-05-13 14:33:16 +0300

    refactor: add main div in layout

--- src/widgets/layouts/main/ui/MainLayout/MainLayout.tsx
+++ src/widgets/layouts/main/ui/MainLayout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
-
+import { Box } from '@mui/material';
 import layoutStyles from '@/widgets/layouts/main/ui/MainLayout/layout.module.css';
 
 import { Footer } from '../Footer/Footer';
@@ -9,12 +9,12 @@ interface MainLayoutProps {}
 
 export const MainLayout = ({ children }: PropsWithChildren<MainLayoutProps>) => {
   return (
-    <div className={layoutStyles.wrapper}>
       <div className={layoutStyles.container}>
         <Header />
+        <Box component="main" className={layoutStyles.page}>
         {children}
+        </Box>
         <Footer />
       </div>
-    </div>
   );
 };

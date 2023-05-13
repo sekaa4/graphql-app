ffd4e2dbc53a25ce567072a2291306d24644e51a
Sergey P <s.pansevi4@gmail.com>
2023-05-13 17:46:36 +0300

    fix: add CustomSchema&CustomSchemaTree into DocumentSchema component

--- src/features/SideBar/ui/DocumentSchema.tsx
+++ src/features/SideBar/ui/DocumentSchema.tsx
@@ -1,17 +1,71 @@
-import { PropsWithChildren } from 'react';
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+import React, { PropsWithChildren } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const SideBar = (props: PropsWithChildren) => {
-  const { children } = props;
+import { CustomSchema } from '@/entities/CustomSchema';
+import { CustomSchemaTree } from '@/entities/CustomSchemaTree';
+import { useAppDispatch, useAppSelector } from '@/shared/hooks';
+import {
+  graphQlSchemaFieldsOperations,
+  graphQlSchemaOperations,
+} from '@/shared/lib/graphQlSchemaOperations/graphQlSchemaOperations';
+
+import { getDocumentationCurPath, getDocumentationPrevPath } from '../model/selectors';
+import { documentationActions } from '../model/slice/documentationSlice';
+interface DocumentSchemaProps {
+  schema?: string;
+  error?: FetchBaseQueryError | SerializedError;
+  isLoading: boolean;
+}
+const DocumentSchema = (props: PropsWithChildren<DocumentSchemaProps>) => {
+  const { children, isLoading, error, schema } = props;
+  const curPath = useAppSelector(getDocumentationCurPath);
+  const prevPath = useAppSelector(getDocumentationPrevPath);
+  const dispatch = useAppDispatch();
   const { t } = useTranslation('common');
+  const path = { curPath, prevPath };
+
+  const handleClickNextPath = (e: React.MouseEvent<HTMLSpanElement>) => {
+    e.preventDefault();
+    const path = e.target as HTMLAnchorElement;
+
+    dispatch(documentationActions.changeCurPath(path.id));
+  };
+
+  const handleClickPrevPath = (e: React.MouseEvent<HTMLSpanElement>) => {
+    e.preventDefault();
+    const path = e.target as HTMLAnchorElement;
+
+    dispatch(documentationActions.changePrevPath(path.id));
+  };
 
   return (
     <div>
-      <div>{t('doc')}</div>
-      <div>{t('descriptionDoc')}</div>
+      {isLoading && <div>LOADING...</div>}
+      {!isLoading && !error && schema && !curPath && (
+        <CustomSchema
+          schema={graphQlSchemaOperations(schema)}
+          handleClickNextPath={handleClickNextPath}
+        />
+      )}
+      {!isLoading && !error && schema && curPath && (
+        <CustomSchemaTree
+          path={path}
+          fields={graphQlSchemaFieldsOperations(schema, curPath)}
+          handleClickNextPath={handleClickNextPath}
+          handleClickPrevPath={handleClickPrevPath}
+        />
+      )}
+      {!isLoading && error && (
+        <>
+          <div>{'Invalid schema, pls try one more time'}</div>
+          <div>{JSON.stringify(error, null, 2)}</div>
+        </>
+      )}
       {children}
     </div>
   );
 };
 
-export default SideBar;
+export default DocumentSchema;
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,23 +1,22 @@
 import { Button } from '@mui/material';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
-import { Suspense, useEffect, useLayoutEffect } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 import { auth, logout } from '@/app/components/FireBase';
-import { CustomSchema } from '@/entities/CustomSchema';
 import { fetchSchemaByAPI } from '@/features/SideBar/api/shemaByAnyAPI';
 import { DocumentSchemaLazy } from '@/features/SideBar/ui/DocumentSchema.lazy';
 import homeStyles from '@/pages/main/main.module.css';
 import { getCoreServerSideProps, SSRPageProps } from '@/shared/lib/ssr';
 import { LangSwitcher } from '@/shared/ui/LangSwitcher/LangSwitcher';
 import { Sidebar } from '@/widgets/layouts/side-bar';
-
-const GRAPHQL_END_POINT_SCHEMA = 'https://rickandmortyapi.com/graphql';
+const GRAPHQL_END_POINT_SCHEMA = 'https://graphql.anilist.co/';
 
 const Home = (props: SSRPageProps) => {
   const [user, loading, error] = useAuthState(auth);
   const router = useRouter();
+  const [isOpen, setStatusOpen] = useState<boolean>(false);
   const { t } = useTranslation('common');
 
   const [getSchemaByAPI, { data: currentSchema, error: errorAPI, isLoading }] = fetchSchemaByAPI();
@@ -44,12 +43,19 @@ const Home = (props: SSRPageProps) => {
         </div>
         <div></div>
       </main>
-      {/* need draw when click on icon */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <DocumentSchemaLazy>
-          <CustomSchema schema={currentSchema} />
-        </DocumentSchemaLazy>
-      </Suspense>
+      <Button
+        variant="contained"
+        onClick={() => {
+          setStatusOpen(!isOpen);
+        }}
+      >
+        Docs
+      </Button>
+      {isOpen && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <DocumentSchemaLazy schema={currentSchema} error={errorAPI} isLoading={isLoading} />
+        </Suspense>
+      )}
     </>
   );
 };

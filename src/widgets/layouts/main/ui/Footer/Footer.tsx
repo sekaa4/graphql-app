24bb3d762d74b80b9db34e6ba850d2ca58a86724
AntonFartovii <anton@rza.by>
2023-05-13 10:35:58 +0300

    feat: add mui elements in footer

--- src/widgets/layouts/main/ui/Footer/Footer.tsx
+++ src/widgets/layouts/main/ui/Footer/Footer.tsx
@@ -1,26 +1,25 @@
 import { FC } from 'react';
+import { Box, Link } from '@mui/material';
 import styles from '@/widgets/layouts/main/ui/Footer/Footer.module.css';
 import rssLogo from '@/shared/icons/rs_school_js.svg';
 import gitLogo from '@/shared/icons/github-logo.svg';
 
 export const Footer: FC = () => {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.rss}>
-        <a href="https://rs.school/js/">
-          <img src={rssLogo.src} alt="RSS logo" />
-        </a>
+  return (    <Box component="footer"
+                   className={styles.footer}>
+    <div className={styles.rss}>
+      <a href="https://rs.school/js/">
+        <img src={rssLogo.src} alt="RSS logo" />
+      </a>
+    </div>
+    <div className={styles.git}>
+      <img src={gitLogo.src} alt="GitHub Logo" />
+      <div className={styles.links}>
+        <Link href="https://github.com/ImmelstronDev">GitHub Pavel</Link>
+        <Link href="https://github.com/sekaa4">GitHub Sergei</Link>
+        <Link href="https://github.com/AntonFartovii">GitHub Anton</Link>
       </div>
-      <div className={styles.git}>
-        <img src={gitLogo.src} alt="GitHub Logo"/>
-        <div className={styles.links}>
-          <a href="https://github.com/ImmelstronDev">GitHub Pavel</a>
-          <a href="https://github.com/sekaa4">GitHub Sergei</a>
-          <a href="https://github.com/AntonFartovii">GitHub Anton</a>
-        </div>
-      </div>
-      <div>
-        © 2023
-      </div>
-    </footer>);
+    </div>
+    <div>© 2023</div>
+  </Box>);
 };

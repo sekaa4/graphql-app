5984ef04b57cffff5e9b6b71f837938afd994cef
Sergey P <s.pansevi4@gmail.com>
2023-05-14 19:09:19 +0300

    fix: add uniq key for map list in CustomSchemaTree component

--- src/entities/CustomSchemaTree/ui/CustomSchemaTree.tsx
+++ src/entities/CustomSchemaTree/ui/CustomSchemaTree.tsx
@@ -48,80 +48,78 @@ export const CustomSchemaTree = (props: CustomSchemaProps) => {
                 fieldsOfConfigTypedObj[nameField].type instanceof GraphQLNonNull;
 
               return (
-                <>
-                  <li>
-                    <div>{fieldsOfConfigTypedObj[nameField].description}</div>
-                    <div>
-                      <span>{nameField}:</span>
+                <li key={v4()}>
+                  <div>{fieldsOfConfigTypedObj[nameField].description}</div>
+                  <div>
+                    <span>{nameField}:</span>
 
-                      {isGraphQLList && !isGraphQLNonNull && (
-                        <span>
-                          [
-                          <a
-                            id={namedType.name}
-                            onClick={handleClickNextPath}
-                            style={{
-                              cursor: 'pointer',
-                              color: 'yellow',
-                              textDecoration: 'underline',
-                            }}
-                          >
-                            {namedType.name}
-                          </a>
-                          ]
-                        </span>
-                      )}
-                      {isGraphQLList && isGraphQLNonNull && (
-                        <span>
-                          [
-                          <a
-                            id={namedType.name}
-                            onClick={handleClickNextPath}
-                            style={{
-                              cursor: 'pointer',
-                              color: 'yellow',
-                              textDecoration: 'underline',
-                            }}
-                          >
-                            {namedType.name}
-                          </a>
-                          !]
-                        </span>
-                      )}
-                      {!isGraphQLList && isGraphQLNonNull && (
-                        <span>
-                          <a
-                            id={namedType.name}
-                            onClick={handleClickNextPath}
-                            style={{
-                              cursor: 'pointer',
-                              color: 'yellow',
-                              textDecoration: 'underline',
-                            }}
-                          >
-                            {namedType.name}
-                          </a>
-                          !
-                        </span>
-                      )}
-                      {!isGraphQLList && !isGraphQLNonNull && (
-                        <span>
-                          <a
-                            id={namedType.name}
-                            onClick={handleClickNextPath}
-                            style={{
-                              cursor: 'pointer',
-                              color: 'yellow',
-                              textDecoration: 'underline',
-                            }}
-                          >
-                            {namedType.name}
-                          </a>
-                        </span>
-                      )}
-                    </div>
-                  </li>
-                </>
+                    {isGraphQLList && !isGraphQLNonNull && (
+                      <span>
+                        [
+                        <a
+                          id={namedType.name}
+                          onClick={handleClickNextPath}
+                          style={{
+                            cursor: 'pointer',
+                            color: 'yellow',
+                            textDecoration: 'underline',
+                          }}
+                        >
+                          {namedType.name}
+                        </a>
+                        ]
+                      </span>
+                    )}
+                    {isGraphQLList && isGraphQLNonNull && (
+                      <span>
+                        [
+                        <a
+                          id={namedType.name}
+                          onClick={handleClickNextPath}
+                          style={{
+                            cursor: 'pointer',
+                            color: 'yellow',
+                            textDecoration: 'underline',
+                          }}
+                        >
+                          {namedType.name}
+                        </a>
+                        !]
+                      </span>
+                    )}
+                    {!isGraphQLList && isGraphQLNonNull && (
+                      <span>
+                        <a
+                          id={namedType.name}
+                          onClick={handleClickNextPath}
+                          style={{
+                            cursor: 'pointer',
+                            color: 'yellow',
+                            textDecoration: 'underline',
+                          }}
+                        >
+                          {namedType.name}
+                        </a>
+                        !
+                      </span>
+                    )}
+                    {!isGraphQLList && !isGraphQLNonNull && (
+                      <span>
+                        <a
+                          id={namedType.name}
+                          onClick={handleClickNextPath}
+                          style={{
+                            cursor: 'pointer',
+                            color: 'yellow',
+                            textDecoration: 'underline',
+                          }}
+                        >
+                          {namedType.name}
+                        </a>
+                      </span>
+                    )}
+                  </div>
+                </li>
               );
             }
           })}
@@ -129,14 +127,12 @@ export const CustomSchemaTree = (props: CustomSchemaProps) => {
           fieldsOfConfigEnumTyped.map((nameField) => {
             const namedField = nameField;
             return (
-              <div key={v4()}>
-                <li>
-                  <div>{namedField.description}</div>
-                  <div>
-                    <span>{namedField.name}</span>
-                  </div>
-                </li>
-              </div>
+              <li key={v4()}>
+                <div>{namedField.description}</div>
+                <div>
+                  <span>{namedField.name}</span>
+                </div>
+              </li>
             );
           })}
       </ul>

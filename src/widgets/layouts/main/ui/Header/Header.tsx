99cfec38ffd804bc0eec61b0824c10084a132d5e
AntonFartovii <anton@rza.by>
2023-05-17 17:30:22 +0300

    feat: swith header depending of scroll

--- src/widgets/layouts/main/ui/Header/Header.tsx
+++ src/widgets/layouts/main/ui/Header/Header.tsx
@@ -1,10 +1,27 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Box, Button } from '@mui/material';
 import styles from '@/widgets/layouts/main/ui/Header/Header.module.css';
 
 export const Header: FC = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      scrollTop > 0 ? setScrolled(true) : setScrolled(false);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const className = !scrolled ? styles.sticky : styles.sticky + ' ' + styles.scroll;
+
   return (
-    <Box component="header" className={styles.sticky}>
+    <Box component="header" className={className}>
       <div className={styles.wrapper}>
         <Button>button</Button>
       </div>

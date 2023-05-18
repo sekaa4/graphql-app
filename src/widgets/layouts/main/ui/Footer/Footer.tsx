284ac68af87533c021910dd440288a0f799307cc
AntonFartovii <anton@rza.by>
2023-05-18 16:01:12 +0300

    feat: comment import svg in Footer

--- src/widgets/layouts/main/ui/Footer/Footer.tsx
+++ src/widgets/layouts/main/ui/Footer/Footer.tsx
@@ -1,19 +1,19 @@
 import { FC } from 'react';
 import { Box, Link } from '@mui/material';
 import styles from '@/widgets/layouts/main/ui/Footer/Footer.module.css';
-import rssLogo from '@/shared/icons/rs_school_js.svg';
-import gitLogo from '@/shared/icons/github-logo.svg';
+// import rssLogo from '@/shared/icons/rs_school_js.svg';
+// import gitLogo from '@/shared/icons/github-logo.svg';
 
 export const Footer: FC = () => {
   return (
     <Box component="footer" className={styles.footer}>
       <div className={styles.rss}>
         <a href="https://rs.school/react/">
-          <img src={rssLogo} alt="RSS logo" />
+          {/*<img src={rssLogo} alt="RSS logo" />*/}
         </a>
       </div>
       <div className={styles.git}>
-        <img src={gitLogo} alt="GitHub Logo" />
+        {/*<img src={gitLogo} alt="GitHub Logo" />*/}
         <div className={styles.links}>
           <Link href="https://github.com/ImmelstronDev">GitHub Pavel</Link>
           <Link href="https://github.com/sekaa4">GitHub Sergei</Link>

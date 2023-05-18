b095718250aa9c56337bc7b4bcc9f9f09458d953
AntonFartovii <anton@rza.by>
2023-05-18 15:57:05 +0300

    refactor: pathes svg in Footer

--- src/widgets/layouts/main/ui/Footer/Footer.tsx
+++ src/widgets/layouts/main/ui/Footer/Footer.tsx
@@ -9,11 +9,11 @@ export const Footer: FC = () => {
     <Box component="footer" className={styles.footer}>
       <div className={styles.rss}>
         <a href="https://rs.school/react/">
-          <img src={rssLogo.src} alt="RSS logo" />
+          <img src={rssLogo} alt="RSS logo" />
         </a>
       </div>
       <div className={styles.git}>
-        <img src={gitLogo.src} alt="GitHub Logo" />
+        <img src={gitLogo} alt="GitHub Logo" />
         <div className={styles.links}>
           <Link href="https://github.com/ImmelstronDev">GitHub Pavel</Link>
           <Link href="https://github.com/sekaa4">GitHub Sergei</Link>

aaffea1defb84fa952901a8d834fed2b855c98be
Sergey P <s.pansevi4@gmail.com>
2023-05-18 17:03:07 +0300

    fix: change tag img to Image in Footer.tsx component

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,5 +1,1 @@
 //write global declarations here
-// declare module '*.svg' {
-//   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-//   export default content;
-// }
--- src/widgets/layouts/main/ui/Footer/Footer.tsx
+++ src/widgets/layouts/main/ui/Footer/Footer.tsx
@@ -1,17 +1,21 @@
-import { FC } from 'react';
 import { Box, Link } from '@mui/material';
+import Image from 'next/image';
+import { FC } from 'react';
+
+import gitLogo from '@/shared/icons/github-logo.svg';
+import rssLogo from '@/shared/icons/rs_school_js.svg';
 import styles from '@/widgets/layouts/main/ui/Footer/Footer.module.css';
-// import rssLogo from '@/shared/icons/rs_school_js.svg';
-// import gitLogo from '@/shared/icons/github-logo.svg';
 
 export const Footer: FC = () => {
   return (
     <Box component="footer" className={styles.footer}>
       <div className={styles.rss}>
-        <a href="https://rs.school/react/">{/*<img src={rssLogo} alt="RSS logo" />*/}</a>
+        <a href="https://rs.school/react/">
+          <Image src={rssLogo} alt="RSS logo" />
+        </a>
       </div>
       <div className={styles.git}>
-        {/*<img src={gitLogo} alt="GitHub Logo" />*/}
+        <Image src={gitLogo} alt="GitHub Logo" />
         <div className={styles.links}>
           <Link href="https://github.com/ImmelstronDev">GitHub Pavel</Link>
           <Link href="https://github.com/sekaa4">GitHub Sergei</Link>

7cb909068cc027288c1ed0829fb69110d8197751
AntonFartovii <anton@rza.by>
2023-05-19 23:36:54 +0300

    feat: resize block left and right in main page

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@mui/material';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
-import { Suspense, useEffect, useState } from 'react';
+import { MouseEvent, Suspense, useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 import { auth, logout } from '@/app/components/FireBase';
@@ -14,12 +14,24 @@ import homeStyles from '@/pages/main/main.module.css';
 import { useAppDispatch, useAppSelector } from '@/shared/hooks';
 import { getCoreServerSideProps, SSRPageProps } from '@/shared/lib/ssr';
 
+type motionState = {
+  active: boolean;
+  deltaLeft: null | number;
+  delta: null | number;
+};
+
 const Home = (props: SSRPageProps) => {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const [user, loading, error] = useAuthState(auth);
   const [isOpen, setStatusOpen] = useState<boolean>(false);
   const [isDisabled, setDisabledButton] = useState<boolean>(true);
+  const [motion, setMotion] = useState<motionState>({
+    active: false,
+    deltaLeft: null,
+    delta: null,
+  });
+  const [width, setWidth] = useState<number | null>(null);
   const curSearchBarInput = useAppSelector(getSearchBarInput);
   const { t } = useTranslation('common');
 
@@ -45,33 +57,78 @@ const Home = (props: SSRPageProps) => {
     }
   }, [currentSchema, dispatch, isLoading]);
 
+  const mouseMove = (e: MouseEvent) => {
+    if (motion.active) {
+      const deltaLeft = motion.deltaLeft ? motion.deltaLeft : 0;
+      const clientX = e.clientX ? e.clientX : 0;
+      const delta = motion.delta ? motion.delta : 0;
+      setWidth(clientX - deltaLeft + delta);
+    }
+  };
+
+  const mouseUp = () => {
+    setMotion((motion) => ({
+      ...motion,
+      active: false,
+    }));
+  };
+
+  const mouseDown = (e: MouseEvent) => {
+    const parent = (e.target as HTMLDivElement).parentElement;
+    const coords: DOMRect | undefined = parent?.getBoundingClientRect();
+    const width = coords?.width ? coords.width : 0;
+    const left = coords?.left ? coords?.left : 0;
+    const delta: number = e.clientX - width - left;
+
+    setMotion((motion) => ({
+      ...motion,
+      active: true,
+      deltaLeft: left,
+      delta: delta,
+    }));
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', mouseMove as any);
+    window.addEventListener('mouseup', mouseUp as any);
+
+    return () => {
+      window.removeEventListener('mousemove', mouseMove as any);
+      window.removeEventListener('mouseup', mouseUp as any);
+    };
+  }, [mouseDown, mouseUp, mouseMove]);
+
   return (
     <div className={homeStyles.wrapper}>
-      <SearchBar isError={isError} isLoading={isLoading} />
-      <div className={homeStyles.textareawrapper}>
-        <Editor />
+      {motion.active}
+      <div className={homeStyles.left} style={{ width: width as any }}>
+        <SearchBar isError={isError} isLoading={isLoading} />
+        <div className={homeStyles.textareawrapper}>
+          <textarea className={homeStyles.textarea}></textarea>
+        </div>
+        <Button
+          variant="contained"
+          disabled={isDisabled}
+          onClick={() => {
+            currentSchema && setStatusOpen(!isOpen);
+          }}
+        >
+          Docs
+        </Button>
+        {isOpen && (
+          <Suspense fallback={<div>Loading...</div>}>
+            <DocumentSchemaLazy schema={currentSchema} />
+          </Suspense>
+        )}
+        {!isLoading && errorAPI && (
+          <>
+            <div>{t('invalidSchema')}</div>
+            <div>{JSON.stringify(errorAPI, null, 2)}</div>
+          </>
+        )}
+        <div className={homeStyles.resizer} onMouseDown={mouseDown} onMouseUp={mouseUp}></div>
       </div>
-      <Button
-        variant="contained"
-        disabled={isDisabled}
-        onClick={() => {
-          currentSchema && setStatusOpen(!isOpen);
-        }}
-      >
-        Docs
-      </Button>
-      {isOpen && (
-        <Suspense fallback={<div>Loading...</div>}>
-          <DocumentSchemaLazy schema={currentSchema} />
-        </Suspense>
-      )}
-      {!isLoading && errorAPI && (
-        <>
-          <div>{t('invalidSchema')}</div>
-          <div>{JSON.stringify(errorAPI, null, 2)}</div>
-        </>
-      )}
-      <div></div>
+      <div className={homeStyles.right}></div>
     </div>
   );
 };

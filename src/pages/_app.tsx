1fe8ced118fd647ae43089f990a77353be62c537
Sergey P <s.pansevi4@gmail.com>
2023-05-23 17:56:21 +0300

    feat: add ErrorBoundary to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,22 @@ import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 
 import { StoreProvider } from '@/app/providers/StoreProvider';
+import ErrorBoundary from '@/shared/ui/ErrorBoundary/ErrorBoundary';
+import { FallBackErrorUI } from '@/shared/ui/FallBackErrorUI/FallBackErrorUI';
 import Preloader from '@/shared/ui/Preloader/Preloader';
 import { MainLayout } from '@/widgets/layouts/main';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <StoreProvider>
-      <Preloader>
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      </Preloader>
-    </StoreProvider>
+    <ErrorBoundary fallBackUIComponent={<FallBackErrorUI />}>
+      <StoreProvider>
+        <Preloader>
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+        </Preloader>
+      </StoreProvider>
+    </ErrorBoundary>
   );
 };
 

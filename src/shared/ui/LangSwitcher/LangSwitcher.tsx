0eaeb62d881f451df42cd2294dc475ff072e3730
Sergey P <s.pansevi4@gmail.com>
2023-05-28 15:59:46 +0300

    fix: change disabled button styles

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -10,25 +10,39 @@ export const LangSwitcher = () => {
   const { t } = useTranslation('common');
   const changeTo = router.locale === 'en' ? 'ru' : 'en';
   const { route } = router;
+  const button = {
+    minWidth: '50px',
+    height: '36.5px',
+    padding: '10px',
+  };
+
+  const desabledButton = {
+    minWidth: '50px',
+    height: '36.5px',
+    padding: '10px',
+    pointerEvents: 'none',
+    color: 'white',
+    backgroundColor: '#4e4e4e',
+  };
 
   return changeTo === 'ru' ? (
     <div className={cls.switch_container}>
       <Link href={route} locale={changeTo}>
-        <Button variant="contained" className={cls.button}>
+        <Button variant="contained" sx={button}>
           {t('locale-ru')}
         </Button>
       </Link>
-      <Button variant="contained" disabled className={cls.button}>
+      <Button variant="contained" sx={desabledButton}>
         {t('locale-eng', { changeTo })}
       </Button>
     </div>
   ) : (
     <div className={cls.switch_container}>
-      <Button variant="contained" disabled className={cls.button}>
+      <Button variant="contained" sx={desabledButton}>
         {t('locale-ru', { changeTo })}
       </Button>
       <Link href={route} locale={changeTo}>
-        <Button variant="contained" className={cls.button}>
+        <Button variant="contained" sx={button}>
           {t('locale-eng')}
         </Button>
       </Link>

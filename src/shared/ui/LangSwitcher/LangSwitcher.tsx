92be2312dea0ef2997461d9b1ae839d3631e4740
Sergey P <s.pansevi4@gmail.com>
2023-05-28 14:34:25 +0300

    fix: change styles in header

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -14,29 +14,21 @@ export const LangSwitcher = () => {
   return changeTo === 'ru' ? (
     <div className={cls.switch_container}>
       <Link href={route} locale={changeTo}>
-        <Button variant="contained" style={{ height: '30px', minWidth: '60px' }}>
+        <Button variant="contained" className={cls.button}>
           {t('locale-ru')}
         </Button>
       </Link>
-      <Button
-        variant="contained"
-        disabled
-        style={{ pointerEvents: 'none', height: '30px', minWidth: '60px' }}
-      >
+      <Button variant="contained" disabled className={cls.button}>
         {t('locale-eng', { changeTo })}
       </Button>
     </div>
   ) : (
     <div className={cls.switch_container}>
-      <Button
-        variant="contained"
-        disabled
-        style={{ pointerEvents: 'none', height: '30px', minWidth: '60px' }}
-      >
+      <Button variant="contained" disabled className={cls.button}>
         {t('locale-ru', { changeTo })}
       </Button>
       <Link href={route} locale={changeTo}>
-        <Button variant="contained" style={{ height: '30px', minWidth: '60px' }}>
+        <Button variant="contained" className={cls.button}>
           {t('locale-eng')}
         </Button>
       </Link>
